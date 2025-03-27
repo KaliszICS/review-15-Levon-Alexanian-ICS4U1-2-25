db3bfcb4e34942319ecae03e15eeaa2f46fe5ddd
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-27 04:03:36 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,38 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static int goodString(String word) {
+        int n = word.length();
+        int minOperations = n; 
+        boolean found = false;
 
-	}
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (word.charAt(i) == word.charAt(j)) {
+                    int operations = i + (n - j - 1);
+                    minOperations = Math.min(minOperations, operations);
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+
+        if (!found) return -1;
+
+        found = false;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (word.charAt(i) == word.charAt(j)) {
+                    int operations = j + (n - i - 1);
+                    minOperations = Math.min(minOperations, operations);
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+
+        return minOperations;
+    }
 
 }
